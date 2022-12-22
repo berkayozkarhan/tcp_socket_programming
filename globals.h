f81a379c1b6ed3c7bd258d0bd1beb47bcf937fbf
Berkay Özkarhan <berkayozkarhan99@gmail.com>
2022-12-22 23:17:11 +0300

    add globals.h

---
+++ globals.h
@@ -0,0 +1,18 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+namespace G {
+
+namespace Server {
+    /// Options
+    /// tcp : tcp socket
+    /// http: rest api enabled server.
+    const static char* config = "tcp";
+    const static int port = 5000;
+}
+
+
+
+}
+
+#endif // GLOBALS_H
