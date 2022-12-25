4ff9cf78efea5c5c9c97af2d1052ae71ae29a9d5
Berkay Özkarhan <berkayozkarhan99@gmail.com>
2022-12-25 23:08:38 +0300

    server done.

--- main.cpp
+++ main.cpp
@@ -27,34 +27,6 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    Database *db = new Database(G::Db::filePath);
-    db->open();
-    /*
-    QList<User> userList = db->getUserList();
-    QList<Bank> bankList = db->getBankList();
-    Bank bank = db->getBank("Bank3");
-    User user = db->getUser("berkay");
-    User user1;
-    user1.setUserName("test_username");
-    user1.setAccountNo("test-account-no");
-    user1.setBalance(100);
-    user1.setBankName("test-bank");
-    user1.setToken("test-token");
-    //db->registerNewUser(user1);
-    //user.setBalance(3456);
-    //db->updateUser(user);
-    //db->updateUserBalance("berkay", 7654);
-    //db->updateUserBank("berkay", "Bank3");
-    //db->updateUserToken("berkay", "new_token_123123");
-    */
-
-    /*
-    User user = db->getUserByAccountNo("asdasd");
-    if(user.isAvailable)
-        qDebug() << "name : " << user.getUserName();
-    */
-
     Server server;
     server.start(G::Server::port);
     return a.exec();
